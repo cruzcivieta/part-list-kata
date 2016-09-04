463795c0c6a000f7d3cb477542e81f61e1046164
Daniel Cruz <daniel.cruz@scmspain.com>
2016-09-04 15:00:42 +0200

    Some refactors

--- src/CruzCivieta/PartList/PartList.php
+++ src/CruzCivieta/PartList/PartList.php
@@ -6,27 +6,70 @@ namespace CruzCivieta\PartList;
 class PartList
 {
 
+    const INDEX_CORRECTION = 1;
+
     public function execute(array $list)
+    {
+        $this->isValidList($list);
+
+        return $this->doExtract($list);
+
+    }
+
+    /**
+     * @param array $list
+     * @return array
+     */
+    public function doExtract(array $list)
+    {
+        $result = [];
+        for ($i = 0; $i < count($list) - self::INDEX_CORRECTION; $i++) {
+            $firstElement = $this->extractFirstElementFrom($list, $i);
+            $secondElement = $this->extractSecondElementFrom($list, $i);
+
+            $result[] = $this->join($firstElement, $secondElement);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $list
+     */
+    public function isValidList(array $list)
     {
         if (count($list) < 2) {
             throw new \RuntimeException();
         }
+    }
 
-        if (count($list) === 2) {
-            return [$list];
-        }
+    /**
+     * @param array $list
+     * @param $i
+     * @return array
+     */
+    public function extractFirstElementFrom(array $list, $i)
+    {
+        return array_slice($list, 0, $i + self::INDEX_CORRECTION);
+    }
 
-        if (count($list) > 2) {
-            $result = [];
-            for ($i = 0; $i < count($list) - 1; $i++) {
-                $firstElement = array_slice($list, 0, $i + 1);
-                $secondElement = array_slice($list, $i + 1);
+    /**
+     * @param array $list
+     * @param $i
+     * @return array
+     */
+    public function extractSecondElementFrom(array $list, $i)
+    {
+        return array_slice($list, $i + self::INDEX_CORRECTION);
+    }
 
-                $result[] = [join(' ', $firstElement), join(' ', $secondElement)];
-            }
-
-            return $result;
-        }
-
+    /**
+     * @param $firstElement
+     * @param $secondElement
+     * @return array
+     */
+    public function join($firstElement, $secondElement)
+    {
+        return [join(' ', $firstElement), join(' ', $secondElement)];
     }
 }
